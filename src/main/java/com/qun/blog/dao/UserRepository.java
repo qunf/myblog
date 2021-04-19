97030a2d803b3c1cdd22b73c596b7c0d2f798139
qunf <884903497@qq.com>
2021-04-19 10:58:21 +0800

    Revert "Initial commit"
    This reverts commit 2ef50ceb297264af5128e0e7c5cd77eb0cae825e.

--- src/main/java/com/qun/blog/dao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.qun.blog.dao;
-
-import com.qun.blog.po.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    User findByUsernameAndPassword(String username,String password);
-}
--- src/main/java/com/qun/blog/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.qun.blog.service;
-
-import com.qun.blog.po.User;
-
-public interface UserService {
-    User checkUser(String username, String password);
-}
